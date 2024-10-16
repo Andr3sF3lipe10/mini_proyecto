23617a2234832275353de6344847dc08537660e3
Andrés Felipe Castrillón Martínez <145281939+Andr3sF3lipe10@users.noreply.github.com>
2024-10-15 23:31:38 -0500

    creacion de interfaces y cambios en cada clase

--- Miniproyecto_1/src/SoldadoRaso.java
+++ Miniproyecto_1/src/SoldadoRaso.java
@@ -1,10 +1,19 @@
-public class SoldadoRaso extends soldado {
+public class SoldadoRaso extends soldado implements OperacionesMilitares {
+    private String misionAsignada;
 
     public SoldadoRaso(String nombre, String id) {
         super(nombre, id);
+        this.rango = "Soldado Raso";
     }
 
-    public void realizarAccion() {
-        System.out.println(nombre + " está siguiendo órdenes.");
+    @Override
+    public void asignarMision(String mision) {
+        this.misionAsignada = mision;
+        System.out.println("Soldado Raso " + nombre + " ha recibido la misión: " + mision);
+    }
+
+    @Override
+    public void reportarEstado() {
+        System.out.println("Soldado Raso " + nombre + " reporta: En posición y listo para la misión.");
     }
 }
