1600ae778b09d270c0780b0e0d68699b851da4ff
BryanPanesso <bryan.panesso@correounivalle.edu.co>
2024-10-15 22:57:06 -0500

    Modificacion de Rango y Soldado

--- Miniproyecto_1/src/Rango.java
+++ Miniproyecto_1/src/Rango.java
@@ -1,12 +1,11 @@
-public abstract class  Rango {
-
+public abstract class Rango extends soldado {
     protected int nivel;
 
-    
-    public Rango(int nivel) {
+    public Rango(String nombre, String id, String rango, int nivel) {
+        super(nombre, id);
+        this.rango = rango; // Sobrescribe el valor por defecto
         this.nivel = nivel;
     }
 
     public abstract void realizarAccion();
-
 }
