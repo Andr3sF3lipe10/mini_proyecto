df27f561cf44f5c1fac8412fd5c26cf238aed9bb
Natyk05 <natalia.martinez.castaneda@correounivalle.edu.co>
2024-12-17 22:45:54 -0500

    implementacion incompleta del mvc

--- Miniproyecto_1/src/App.java
+++
@@ -1,9 +0,0 @@
-
-public class App {
-    public static void main(String[] args) {
-        // Ejecutar la interfaz gráfica
-        java.awt.EventQueue.invokeLater(() -> {
-            new MainFrame().setVisible(true);
-        });
-    }
-}
--- Miniproyecto_1/src/militar/operaciones/OperacionesMilitares.java
+++
@@ -1,7 +0,0 @@
-package militar.operaciones;
-
-public interface OperacionesMilitares {
-    String asignarMision(String mision);
-    void reportarEstado();
-    String realizarAccion();
-}
