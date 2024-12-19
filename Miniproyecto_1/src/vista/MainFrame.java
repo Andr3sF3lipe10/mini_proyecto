2de8e3bd5de2e9b1c3feb907e4291517c5d1e986
Andrés Felipe Castrillón Martínez <145281939+Andr3sF3lipe10@users.noreply.github.com>
2024-12-18 23:38:21 -0500

    Implementacion de jpopmenu

--- Miniproyecto_1/src/vista/MainFrame.java
+++ Miniproyecto_1/src/vista/MainFrame.java
@@ -1,4 +1,5 @@
 package vista;
+
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
@@ -6,7 +7,6 @@ import javax.swing.border.TitledBorder;
 
 import modelo.rangos.Rango;
 import modelo.soldados.*;
-
 
 /**
  * Clase principal que representa la interfaz gráfica de la aplicación.
@@ -26,9 +26,8 @@ public class MainFrame extends JFrame {
     private JMenuBar menuBar;
     private JMenu menuArchivo;
     private JMenuItem menuItemSalir;
-    
-
-
+    private JPopupMenu popupMenu;
+    private JMenuItem menuItemVerDetalles, menuItemEliminar;
 
     // Lista por defecto de soldados
     private java.util.List<Rango> listaSoldadosPorDefecto;
@@ -50,21 +49,20 @@ public class MainFrame extends JFrame {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    
+
         setTitle("Sistema Militar");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800, 600);
         setLocationRelativeTo(null);
 
-         // Menú
-         menuBar = new JMenuBar();
-         menuArchivo = new JMenu("Archivo");
-         menuItemSalir = new JMenuItem("Salir");
-         menuArchivo.add(menuItemSalir);
-         menuBar.add(menuArchivo);
-         setJMenuBar(menuBar);
- 
-    
+        // Menú
+        menuBar = new JMenuBar();
+        menuArchivo = new JMenu("Archivo");
+        menuItemSalir = new JMenuItem("Salir");
+        menuArchivo.add(menuItemSalir);
+        menuBar.add(menuArchivo);
+        setJMenuBar(menuBar);
+
         // Modelo y lista de soldados
         modeloSoldados = new DefaultListModel<>();
         listSoldados = new JList<>(modeloSoldados);
@@ -75,12 +73,12 @@ public class MainFrame extends JFrame {
 
         JScrollPane scrollPaneList = new JScrollPane(listSoldados);
         scrollPaneList.getViewport().setBackground(new Color(96, 108, 56)); // Fondo visible
-        scrollPaneList.setBorder(new TitledBorder(new EmptyBorder(5, 5, 5, 5), "Soldados", TitledBorder.CENTER, TitledBorder.TOP, new Font("Arial", Font.BOLD, 12), new Color(254, 250, 224)));
+        scrollPaneList.setBorder(new TitledBorder(new EmptyBorder(5, 5, 5, 5), "Soldados", TitledBorder.CENTER,
+                TitledBorder.TOP, new Font("Arial", Font.BOLD, 12), new Color(254, 250, 224)));
         scrollPaneList.setPreferredSize(new Dimension(250, 0));
-        
+
         scrollPaneList.setBackground(new Color(96, 108, 56));
-        
-    
+
         // Panel derecho con diseño GridBagLayout
         JPanel panelDerecho = new JPanel(new GridBagLayout());
         panelDerecho.setBorder(new EmptyBorder(10, 10, 10, 10));
@@ -88,15 +86,15 @@ public class MainFrame extends JFrame {
         panelDerecho.setPreferredSize(new Dimension(300, 0));
         panelDerecho.setMaximumSize(new Dimension(300, Integer.MAX_VALUE));
         panelDerecho.setMinimumSize(new Dimension(300, 0));
-        panelDerecho.setBorder(new TitledBorder(new EmptyBorder(10, 10, 10, 10), "Detalles del Soldado", TitledBorder.CENTER, TitledBorder.TOP, new Font("Arial", Font.BOLD, 12), new Color(0,0,0)));
-        
+        panelDerecho.setBorder(new TitledBorder(new EmptyBorder(10, 10, 10, 10), "Detalles del Soldado",
+                TitledBorder.CENTER, TitledBorder.TOP, new Font("Arial", Font.BOLD, 12), new Color(0, 0, 0)));
+
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(5, 5, 5, 5); // Espaciado entre componentes
         gbc.anchor = GridBagConstraints.WEST; // Alineación a la izquierda
         gbc.fill = GridBagConstraints.HORIZONTAL; // Expansión horizontal
         gbc.weightx = 1.0; // Peso de expansión
-        
-    
+
         // Crear y agregar componentes
         lblNombre = new JLabel("Nombre: ");
         lblNombre.setFont(new Font("Arial", Font.BOLD, 12));
@@ -111,7 +109,8 @@ public class MainFrame extends JFrame {
         txtAreaAcciones.setLineWrap(true);
         txtAreaAcciones.setEditable(false);
         JScrollPane scrollPaneTextArea = new JScrollPane(txtAreaAcciones);
-        scrollPaneTextArea.setBorder(new TitledBorder(new EmptyBorder(5, 5, 5, 5), "Acciones", TitledBorder.CENTER, TitledBorder.TOP, new Font("Arial", Font.BOLD, 12), new Color(0,0,0)));
+        scrollPaneTextArea.setBorder(new TitledBorder(new EmptyBorder(5, 5, 5, 5), "Acciones", TitledBorder.CENTER,
+                TitledBorder.TOP, new Font("Arial", Font.BOLD, 12), new Color(0, 0, 0)));
         btnAsignarMision = new JButton("Asignar Misión");
         btnAsignarMision.setBackground(new Color(254, 250, 224));
         btnRealizarAccion = new JButton("Realizar Acción");
@@ -124,22 +123,33 @@ public class MainFrame extends JFrame {
         chkPrepararse.setBackground(new Color(254, 250, 224));
         rbtnReporteEstado = new JRadioButton("Reportar Estado");
         rbtnReporteEstado.setBackground(new Color(254, 250, 224));
-    
-        // Agregar componentes al pane  l derecho
-        gbc.gridx = 0; gbc.gridy = 0; panelDerecho.add(lblNombre, gbc);
-        gbc.gridy = 1; panelDerecho.add(lblID, gbc);
-        gbc.gridy = 2; panelDerecho.add(lblRango, gbc);
-        gbc.gridy = 3; panelDerecho.add(lblInfoEspecifica, gbc);
-        gbc.gridy = 4; panelDerecho.add(new JLabel("Misión: "), gbc);
-        gbc.gridy = 5; panelDerecho.add(txtMision, gbc);
-        gbc.gridy = 6; panelDerecho.add(btnAsignarMision, gbc);
-        gbc.gridy = 7; panelDerecho.add(btnRealizarAccion, gbc);
-        gbc.gridy = 8; panelDerecho.add(chkPrepararse, gbc);
-        gbc.gridy = 9; panelDerecho.add(rbtnReporteEstado, gbc);
-        gbc.gridy = 10; panelDerecho.add(btnCrearSoldado, gbc);
-        gbc.gridy = 11; panelDerecho.add(btnReset, gbc);
-    
-        
+
+        // Agregar componentes al pane l derecho
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        panelDerecho.add(lblNombre, gbc);
+        gbc.gridy = 1;
+        panelDerecho.add(lblID, gbc);
+        gbc.gridy = 2;
+        panelDerecho.add(lblRango, gbc);
+        gbc.gridy = 3;
+        panelDerecho.add(lblInfoEspecifica, gbc);
+        gbc.gridy = 4;
+        panelDerecho.add(new JLabel("Misión: "), gbc);
+        gbc.gridy = 5;
+        panelDerecho.add(txtMision, gbc);
+        gbc.gridy = 6;
+        panelDerecho.add(btnAsignarMision, gbc);
+        gbc.gridy = 7;
+        panelDerecho.add(btnRealizarAccion, gbc);
+        gbc.gridy = 8;
+        panelDerecho.add(chkPrepararse, gbc);
+        gbc.gridy = 9;
+        panelDerecho.add(rbtnReporteEstado, gbc);
+        gbc.gridy = 10;
+        panelDerecho.add(btnCrearSoldado, gbc);
+        gbc.gridy = 11;
+        panelDerecho.add(btnReset, gbc);
 
         // SplitPane con panel izquierdo y derecho
         splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scrollPaneList, panelDerecho);
@@ -148,14 +158,24 @@ public class MainFrame extends JFrame {
         splitPane.setResizeWeight(0.5);
         splitPane.setBorder(null);
         splitPane.setBackground(new Color(254, 250, 224));
-    
+
         // Agregar componentes al frame
         getContentPane().setLayout(new BorderLayout());
         getContentPane().add(splitPane, BorderLayout.CENTER);
         getContentPane().add(scrollPaneTextArea, BorderLayout.SOUTH);
         getContentPane().setBackground(new Color(254, 250, 224));
         getContentPane().setPreferredSize(getPreferredSize());
+
+         // Configuración del JPopupMenu
+         popupMenu = new JPopupMenu();
+         menuItemEliminar = new JMenuItem("Eliminar Soldado");
+ 
+         popupMenu.add(menuItemEliminar);
+ 
+         // Asocia el JPopupMenu a la lista de soldados
+         listSoldados.setComponentPopupMenu(popupMenu);
     }
+
     /**
      * Carga la lista por defecto de soldados.
      */
@@ -198,6 +218,17 @@ public class MainFrame extends JFrame {
         // Menú Salir
 
         menuItemSalir.addActionListener(e -> System.exit(0));
+
+        // Acción "Eliminar Soldado"
+        menuItemEliminar.addActionListener(e -> {
+            Rango soldadoSeleccionado = listSoldados.getSelectedValue();
+            if (soldadoSeleccionado != null) {
+                modeloSoldados.removeElement(soldadoSeleccionado);
+                txtAreaAcciones.append("Soldado eliminado: " + soldadoSeleccionado.getNombre() + "\n");
+            } else {
+                JOptionPane.showMessageDialog(this, "Seleccione un soldado para eliminar.");
+            }
+        });
     }
 
     /**
@@ -211,7 +242,8 @@ public class MainFrame extends JFrame {
             lblInfoEspecifica.setText("");
 
             if (soldado instanceof Capitan) {
-                lblInfoEspecifica.setText("Soldados bajo mando: " + ((Capitan) soldado).getCantidadSoldadosBajoSuMando());
+                lblInfoEspecifica
+                        .setText("Soldados bajo mando: " + ((Capitan) soldado).getCantidadSoldadosBajoSuMando());
             } else if (soldado instanceof Teniente) {
                 lblInfoEspecifica.setText("Unidad: " + ((Teniente) soldado).getUnidad());
             } else if (soldado instanceof Coronel) {
@@ -293,6 +325,7 @@ public class MainFrame extends JFrame {
             ex.printStackTrace();
         }
     }
+
     public JButton getBtnAsignarMision() {
         return btnAsignarMision;
     }
@@ -324,23 +357,20 @@ public class MainFrame extends JFrame {
     public JTextArea getTxtAreaAcciones() {
         return txtAreaAcciones;
     }
+
     public JCheckBox getChkPrepararse() {
         return chkPrepararse;
     }
-    
+
     public JRadioButton getRbtnReporteEstado() {
         return rbtnReporteEstado;
     }
-    
+
     public void setModeloSoldados(DefaultListModel<Rango> modelo) {
         this.modeloSoldados = modelo;
         listSoldados.setModel(modelo); // Configura el JList con el modelo compartido
         listSoldados.repaint(); // Fuerza la actualización de la lista
     }
-    
-    
-    
-
 
     /**
      * Método principal para ejecutar la aplicación.
